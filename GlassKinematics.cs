6a726f4e6475e9224cdb740ca399a7bbd7f39595
eeeJJaaa <40314611+eeeJJaaa@users.noreply.github.com>
2018-08-19 16:17:13 +1200

    Add files via upload

---
+++ GlassKinematics.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassKinematics : MonoBehaviour   //// THIS SCRIPT IS PLACED ON THE BROKEN SHARDS PREFABS
+{
+
+    public float delay = 0.09f; //good timing for an explosive force of 20 on bombs
+
+    IEnumerator Start()
+    {
+
+        Rigidbody thisRigidbody = GetComponent<Rigidbody>();
+        Collider thisCollider = GetComponent<Collider>();
+
+
+        yield return new WaitForSeconds(delay);
+
+        thisRigidbody.isKinematic = false;
+        thisCollider.enabled = true;
+        
+
+
+    }
+
+   
+    void Update()
+    {
+
+    }
+}
+
+
+
+// Should try and set it up so that if the impact is below a certain force the glass only cracks
+// at the moment the glass breaks on any trigger because i replaced the on collision method with ontrigger method of the main panel
+// and set a delay of when the shards of glass become rigidbodies and when the collider is enabled
+
+
+    // ended up placing another collider on the window, slightly bigger than the original, and making it a trigger....so just walking up to it hits the original collider and the  
+    // window doesnt break, but if you are going over a certain speed the trigger will break the window and delay the rigidbodies etc
---
+++ PlayerInteract.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class PlayerInteract : MonoBehaviour
+{
+
+    public GameObject currentInteractable = null;
+
+    Trigger triggerTrapSlot;
+    PlayerInventory playerInventory;
+
+    //string trapSlot;
+
+
+
+    private void Update()
+    {
+        
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)  // Only one of the two objects needs to have a trigger but the other needs at least a collider
+    {
+        if (other.CompareTag("Interactable"))  //checks to see if the object the player has triggered with is an "Interactable"
+        {
+            Debug.Log(other.name);
+
+            currentInteractable = other.gameObject; // loads the current interactable object into currentInteractable so it can be accessed
+        }
+
+                   
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Interactable"))  //checks to see if the object the player has triggered with is an "Interactable"
+        {
+            if (other.gameObject == currentInteractable) //checks to see if the trigger weve just left is of the current object we are interacting with
+            {
+                currentInteractable = null; //if it was the current object then it is removed
+            }
+
+           
+        }
+    }
+
+    public void ButtonClicked(string trapType) //called from the SENDMESSAGE in "PlayerInventory" script and a string is sent and stored in "trapType"
+    {
+        Debug.Log("Button was CLICKED");
+        Debug.Log(trapType);
+
+        if (currentInteractable != null)
+        {
+            if (currentInteractable.CompareTag("Interactable"))
+            {
+                triggerTrapSlot = currentInteractable.GetComponent<Trigger>(); // gets the script Trigger from the interactable 
+
+
+                if (triggerTrapSlot.trapSlot == "")
+                {
+                    Debug.Log("trap " + trapType + " has been planeted");
+                    triggerTrapSlot.trapSlot = trapType;
+
+                    playerInventory = gameObject.GetComponentInChildren<PlayerInventory>();  //
+                    playerInventory.playerInventoryList.Remove(trapType); //removes the matching string of trapty[e from playerInventoryList in the PlayerInventory script // wont work if multiple traps of the
+                                                                          // same type are in the list?? or maybe it doesnt matter?
+                  
+                }
+                else
+                {
+                    Debug.Log("YOU SHOULD HAVE TRIGGERED A TRAP?????");
+                }
+
+
+
+            }
+
+        }
+
+
+
+    }
+
+   
+}
